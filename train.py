41c4bba1c0189df29ff500625f1d18fa05a84d93
Khalil Nouisser <khalil.nouisser@peaksource.tn>
2019-03-24 12:46:14 +0100

    khalil - training the model

--- train.py
+++ train.py
@@ -1,25 +1,28 @@
-from keras.layers import Dense, Conv2D, Flatten, Dropout, Conv3D
+from keras.layers import Dense, Conv2D, Dropout, Flatten
 from keras.models import Sequential
+from keras.optimizers import Adam
 
-from preprocess import prepareData
+from preprocess import prepareData, PATCH_SIZE
 
 
-def network(width, height, layers):
-    C1 = 3 * layers
-
+def network(layers):
     # Define the model
     model = Sequential()
-    #model.add(Conv2D(C1, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(Conv2D(3 * C1, (3, 3), activation='relu'))
+    model.add(Conv2D(3 * layers, (3, 3), activation='relu', input_shape=(PATCH_SIZE, PATCH_SIZE, layers)))
+    model.add(Conv2D(6 * layers, (3, 3), activation='relu'))
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(6 * layers, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(16, activation='softmax'))
+    model.add(Dense(9, activation='softmax'))
     model.summary()
     return model
 
 
 if __name__ == '__main__':
-    train_image, train_labels, test_image, test_labels = prepareData()
-    #model = network(train_image.shape[0], train_image.shape[1], train_image.shape[2])
+    X_train, y_train, X_test, y_test = prepareData()
+    # generate the model
+    model = network(X_train.shape[3])
+    # Train the model
+    model.compile(Adam(lr=.0005), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100)
